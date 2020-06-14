eb9f88c3c40ec1cf772b6c067b19c850ea91141d
Soohyeon Park <soohyeon317@gmail.com>
2020-06-14 16:33:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/bisket/BisketApplication.java
@@ -0,0 +1,13 @@
+package com.bisket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BisketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bisket/BisketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
